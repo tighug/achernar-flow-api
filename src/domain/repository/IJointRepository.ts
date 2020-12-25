09593af41c77d04aa1afa49af5d5a911fe7114a9
tighug <kapuriko123@gmail.com>
2020-12-25 06:06:02 +0000

    refactor: Change arg types in IRepository

--- src/domain/repository/IJointRepository.ts
+++ src/domain/repository/IJointRepository.ts
@@ -1,6 +1,5 @@
-import { Feeder } from "../model/Feeder";
 import { Joint } from "../model/Joint";
 
 export interface IJointRepository {
-  listByFeeder(feeder: Feeder): Promise<Joint[]>;
+  listByFeederId(feederId: number): Promise<Joint[]>;
 }
--- src/domain/repository/ILineRepository.ts
+++ src/domain/repository/ILineRepository.ts
@@ -1,6 +1,5 @@
-import { Feeder } from "../model/Feeder";
 import { Line } from "../model/Line";
 
 export interface ILineRepository {
-  listByFeeder(Feeder: Feeder): Promise<Line[]>;
+  listByFeederId(feederId: number): Promise<Line[]>;
 }
