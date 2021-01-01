c87b48d67b68b8f92873e0203fab09b1e40bf1bb
tighug <kapuriko123@gmail.com>
2021-01-01 01:33:19 +0000

    feat: Change orm from TypeORM to Prisma

--- src/infrastructure/router.ts
+++ src/infrastructure/router.ts
@@ -1,5 +1,4 @@
 import express, { NextFunction, Request, Response } from "express";
-import { createConnection } from "typeorm";
 import { FeederController } from "../interface/controller/FeederController";
 import { NodeController } from "../interface/controller/NodeController";
 import { LineController } from "../interface/controller/LineController";
@@ -9,42 +8,42 @@ import { LineRepository } from "../interface/gateway/LineRepository";
 import { FeederListInteractor } from "../usecase/feeder/list/FeederListInteractor";
 import { NodeListInteractor } from "../usecase/node/list/NodeListInteractor";
 import { LineListInteractor } from "../usecase/line/list/LineListInteractor";
+import { PrismaClient } from "@prisma/client";
 
 const router = express.Router();
+const prisma = new PrismaClient();
 
-createConnection("development").then((connection) => {
-  const feederRepository = new FeederRepository(connection);
-  const nodeRepository = new NodeRepository(connection);
-  const lineRepository = new LineRepository(connection);
+const feederRepository = new FeederRepository(prisma);
+const nodeRepository = new NodeRepository(prisma);
+const lineRepository = new LineRepository(prisma);
 
-  const feederList = new FeederListInteractor(feederRepository);
-  const nodeList = new NodeListInteractor(nodeRepository);
-  const lineList = new LineListInteractor(lineRepository);
+const feederList = new FeederListInteractor(feederRepository);
+const nodeList = new NodeListInteractor(nodeRepository);
+const lineList = new LineListInteractor(lineRepository);
 
-  const feederController = new FeederController(feederList);
-  const nodeController = new NodeController(nodeList);
-  const lineController = new LineController(lineList);
+const feederController = new FeederController(feederList);
+const nodeController = new NodeController(nodeList);
+const lineController = new LineController(lineList);
 
-  router.get(
-    "/feeders",
-    async (req: Request, res: Response, next: NextFunction) => {
-      await feederController.list(req, res, next);
-    }
-  );
+router.get(
+  "/feeders",
+  async (req: Request, res: Response, next: NextFunction) => {
+    await feederController.list(req, res, next);
+  }
+);
 
-  router.get(
-    "/nodes",
-    async (req: Request, res: Response, next: NextFunction) => {
-      await nodeController.list(req, res, next);
-    }
-  );
+router.get(
+  "/nodes",
+  async (req: Request, res: Response, next: NextFunction) => {
+    await nodeController.list(req, res, next);
+  }
+);
 
-  router.get(
-    "/lines",
-    async (req: Request, res: Response, next: NextFunction) => {
-      await lineController.list(req, res, next);
-    }
-  );
-});
+router.get(
+  "/lines",
+  async (req: Request, res: Response, next: NextFunction) => {
+    await lineController.list(req, res, next);
+  }
+);
 
 export default router;
