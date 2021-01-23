4cb884fd089ac48bcd2d3d7fe7fc2950555af927
tighug <kapuriko123@gmail.com>
2021-01-23 10:33:34 +0900

    chore: Remove mysql/

--- mysql/initdb.d/2_load.sql
+++
@@ -1,8 +0,0 @@
-USE flow;
-LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/feeders.csv" INTO TABLE feeders FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
-LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/nodes.csv" INTO TABLE `nodes` FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
-LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/lines.csv" INTO TABLE `lines` FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
-LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/load_samples.csv" INTO TABLE load_samples FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
-LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/pv_samples.csv" INTO TABLE pv_samples FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
-LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/ehp_samples.csv" INTO TABLE ehp_samples FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
-LOAD DATA LOCAL INFILE "/docker-entrypoint-initdb.d/uchp_samples.csv" INTO TABLE uchp_samples FIELDS TERMINATED BY "," ENCLOSED BY "" LINES TERMINATED BY "\n";
