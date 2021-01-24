900d626f1a3c083c71bee92590d5a81af329cd71
tighug <kapuriko123@gmail.com>
2021-01-25 02:02:17 +0900

    feat: Add support for field selection

--- src/domain/repository/IFeederRepository.ts
+++ src/domain/repository/IFeederRepository.ts
@@ -1,5 +1,5 @@
 import { Feeder } from "../model/Feeder";
 
 export interface IFeederRepository {
-  findAll(): Promise<Feeder[]>;
+  findAll(props: { fields: string[] }): Promise<Partial<Feeder>[]>;
 }
