aea74298e9ef4cf244081cd7831f2638b53f66d1
tighug <kapuriko123@gmail.com>
2021-01-27 20:54:49 +0900

    feat: Add FlowSerializer.ts

---
+++ src/interface/serializer/FlowSerializer.ts
@@ -0,0 +1,34 @@
+import { Flow } from "../../domain/model/Flow";
+import { CaseBaseRO } from "./CaseSerializer";
+import { LineBaseRO } from "./LineSerializer";
+
+export class FlowSerializer {
+  static serializeArray(models: Partial<Flow>[]): FlowsRO {
+    const flows = models.map(this.serializeSingle);
+    return {
+      flowCount: flows.length,
+      flows,
+    };
+  }
+
+  private static serializeSingle(flow: Partial<Flow>): FlowBaseRO {
+    return { ...flow };
+  }
+}
+
+export type FlowBaseRO = Readonly<
+  Partial<{
+    id: number;
+    case: CaseBaseRO;
+    line: LineBaseRO;
+    nextNodeP: number;
+    nextNodeV: number;
+    lineI: number;
+    before: boolean;
+  }>
+>;
+
+export type FlowsRO = Readonly<{
+  flowCount: number;
+  flows: FlowBaseRO[];
+}>;
