ac70bfde471420f56ac2da7e4f4b8cb6840e81d7
tighug <kapuriko123@gmail.com>
2021-01-28 01:58:44 +0900

    fix: Update Load.ts

--- src/domain/model/Load.ts
+++ src/domain/model/Load.ts
@@ -1,12 +1,15 @@
+import { Case } from "./Case";
 import { Node } from "./Node";
 
 export class Load {
-  readonly id?: number;
+  id?: number;
+  readonly case: Case;
   readonly node: Node;
   readonly val: number;
   readonly type: string;
 
   constructor(props: Omit<Load, "id">) {
+    this.case = props.case;
     this.node = props.node;
     this.val = props.val;
     this.type = props.type;
