22801ecdfb3c85bc57748d4168f15bccc25c52c8
tighug <kapuriko123@gmail.com>
2021-01-27 20:51:25 +0900

    feat: Update the model of Case

--- src/domain/model/Case.ts
+++ src/domain/model/Case.ts
@@ -2,21 +2,24 @@ import { Feeder } from "./Feeder";
 
 export class Case {
   id?: number;
-  feeder: Feeder;
+  feeder?: Feeder;
   hour: number;
   minute: number;
   pvCount: number;
   pvScale: number;
   loadScale: number;
+  baseV: number;
   seed: number;
+  status = "waiting";
 
-  constructor(props: Omit<Case, "id">) {
+  constructor(props: Omit<Case, "id" | "status">) {
     this.feeder = props.feeder;
     this.hour = props.hour;
     this.minute = props.minute;
     this.pvCount = props.pvCount;
     this.pvScale = props.pvScale;
     this.loadScale = props.loadScale;
+    this.baseV = props.baseV;
     this.seed = props.seed;
   }
 }
