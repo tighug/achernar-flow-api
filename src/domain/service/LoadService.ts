8e48057c3041e88667861aa2ab2e29af74572b4d
tighug <kapuriko123@gmail.com>
2021-01-27 20:53:31 +0900

    feat: Add LoadService.ts

---
+++ src/domain/service/LoadService.ts
@@ -0,0 +1,80 @@
+import { Load } from "../model/Load";
+import { ISampleRepository } from "../repository/ISampleRepository";
+import { INodeRepository } from "../repository/INodeRepository";
+import { Random } from "./Random";
+import { Node } from "../model/Node";
+import { Sample } from "../model/Sample";
+import { Case } from "../model/Case";
+
+export class LoadService {
+  constructor(
+    private readonly sampleRepository: ISampleRepository,
+    private readonly nodeRepository: INodeRepository
+  ) {}
+
+  async getLoadsAndPVs({
+    feeder,
+    hour,
+    minute,
+    pvCount,
+    pvScale,
+    loadScale,
+    seed,
+  }: Required<Case>): Promise<[Load[], Load[]]> {
+    const rand = new Random(seed);
+    const [loadSamples, pvSamples, nodes] = await Promise.all([
+      this.sampleRepository.findMany({
+        hour,
+        minute,
+        season: "summer",
+        type: "load",
+        fields: [],
+      }) as Promise<Required<Sample>[]>,
+      this.sampleRepository.findMany({
+        hour,
+        minute,
+        season: "summer",
+        type: "pv",
+        fields: [],
+      }) as Promise<Required<Sample>[]>,
+      this.nodeRepository.findMany({
+        feederId: feeder.id,
+        fields: [],
+      }) as Promise<Required<Node>[]>,
+    ]);
+    const houses = nodes.filter((n) => n.hasLoad);
+    const loads = Array.from(
+      Array(houses.length),
+      () => loadSamples[rand.getBetween(0, loadSamples.length - 1)]
+    ).map(
+      (load, i) =>
+        new Load({
+          node: houses[i],
+          val: load.val * loadScale,
+          type: "load",
+        })
+    );
+    const shuffledHouses = this.shuffle(houses);
+    const pvs = Array.from(
+      Array(pvCount),
+      () => pvSamples[rand.getBetween(0, pvSamples.length - 1)]
+    ).map(
+      (pv, i) =>
+        new Load({
+          node: shuffledHouses[i],
+          val: pv.val * pvScale,
+          type: "pv",
+        })
+    );
+
+    return [loads, pvs];
+  }
+
+  private shuffle([...array]) {
+    for (let i = array.length - 1; i >= 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+}
