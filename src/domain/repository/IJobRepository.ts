8afdf9d7f40da4f83fa69c6c246174dafd42663b
tighug <kapuriko123@gmail.com>
2021-01-31 01:18:22 +0900

    fix: Update process()

--- src/domain/repository/IJobRepository.ts
+++ src/domain/repository/IJobRepository.ts
@@ -4,10 +4,7 @@ import { SimulateJob } from "../model/SimulateJob";
 export interface IJobRepository {
   add(caseId: number): Promise<SimulateJob>;
   count(): Promise<JobCounts>;
-  process(
-    name: string,
-    callback: ProcessCallbackFunction<{ caseId: number }>
-  ): void;
+  process(callback: ProcessCallbackFunction<{ caseId: number }>): Promise<void>;
   on(event: string, callback: (...args: any) => void): Queue;
   notify(id: number, status: string): void;
 }
